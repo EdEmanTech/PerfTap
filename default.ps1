e10a8765b003aa7c5cc70b0b1da7041d6d9ff783
Emmanuel Acheampong <emmanuel_a2k@yahoo.com>
2013-10-05 16:27:39 +0100

    Added teamcity file and updated the build file and  added psake tools folder to facilitate teamcity builds till i switch to parameter-based master build ps1 file

--- default.ps1
+++ default.ps1
@@ -1,3 +1,11 @@
+<#  
+    Authored by Emmanuel Acheampong 
+    Based on articles such as : 
+        http://visualstudiomagazine.com/articles/2011/07/28/wcoss_coded-builds.aspx
+        http://ayende.com/blog/4156/on-psake
+        https://gist.github.com/toddb/1133511
+#>
+
 	properties { 
 	  $project = "PerfTap"
 	  
@@ -18,32 +26,57 @@
 
 	  $output_dir = "$base_dir\output"
 	  $release_dir = "$output_dir\Releases"
-	  $extract_dir = "$base_dir\CodeToDeploy\Deploy"
-	  $buildartifacts_dir = "$build_dir\CodeToDeploy\Publish"
+	  $package_dir = "$output_dir\Packages"	  
 	  
 	  $test_runner_path = "$tools_dir\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
 	  $test_runner_opts = "$output_dir\xunit.results.html"
 
   	  $test_dir = "$src_dir\$project.Tests\$configuration"
-	  $test_path = "$output_dir\$project.Tests.dll"
+	  $test_path = "$release_dir\$project.Tests.dll"
 	   
 	  $sln_file = "$src_dir\$project.sln"
 	}
+#
+ #   Include  .\teamcity.psm1
+  #  TaskSetup {
+   #     TeamCity-ReportBuildProgress "Running task $($script:context.Peek().currentTaskName)"
+    #}
 
-
-	Task Clean {
+    task Clean { 
+        remove-item -force -recurse $output_dir -ea SilentlyContinue | Out-Null
+        remove-item -force -recurse $release_dir -ea SilentlyContinue | Out-Null
+        remove-item -force -recurse $package_dir -ea SilentlyContinue | Out-Null
         exec { msbuild $sln_file /t:clean /verbosity:$verbosity }
     }
 
-	Task Compile  {
-		exec { msbuild $sln_file /t:build "/p:Configuration=$config" "/p:Platform=$platform" /p:OutDir="$output_dir" }
-	}
-
-	Task Test -depends Compile {
-		assert(Test-Path($test_runner_path)) "xUnit must be available."
-		assert(Test-Path($test_path)) "PerfTap.Test Path must be available."
-        Exec { cmd /C $test_runner_path $test_path /html $test_runner_opts }	
+	
+    task Init -depends Clean {
+        new-item $release_dir -itemType directory | Out-Null 
+        new-item $package_dir -itemType directory | Out-Null       
     }
 
+
+	task Compile -depends Init {
+		exec { msbuild $sln_file /t:rebuild "/p:Configuration=$config" "/p:Platform=$platform" /p:OutDir="$release_dir" }
+	}
+
+
+	task Test -depends Compile {
+		assert(Test-Path($test_runner_path)) "xUnit must be available."
+		assert(Test-Path($test_path)) "PerfTap.Test Path must be available."
+        $testassemblies = get-childitem $release_dir -recurse -include *tests*.dll
+        exec { 
+            & $test_runner_path $testassemblies /Teamcity /html $test_runner_opts;
+        }
+    }
+
+    task IntegrationTest -depends Test { 
+        assert(Test-Path($test_runner_path)) "xUnit must be available."
+        exec { cmd /C $test_runner_path $test_path /html $test_runner_opts }
+    }
+
+    Task Build -depends Compile
+    Task Default -depends Build
+    Task Package -depends Default, IntegrationTest
 
 
